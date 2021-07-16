7ecdb5383fcd524a7697d784fa3e72c3bcd7863b
HarikaValikala <harikavalikala521@gmail.com>
2021-07-16 21:54:41 +0530

    Route Redirect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Link, NavLink, Redirect } from 'react-router-dom';
 import ReactElementComponent from './components/ReactElementPage';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  function onClickHandle() {
+    setLoggedIn(!loggedIn);
+  }
   return (
     <BrowserRouter>
       <div className="App">
@@ -16,16 +21,22 @@ function App() {
               <NavLink className="App-link" to="/elements" exact activeClassName="link-active-style">Elements</NavLink>
             </li>
             <li>
-              <NavLink className="App-link" to="/user/harika/valikala" exact activeClassName="link-active-style">Users</NavLink>
+              <NavLink className="App-link" to="/user/Harika/Valikala" exact activeClassName="link-active-style">Users</NavLink>
             </li>
           </ul>
+          {loggedIn.toString()}
+          <button onClick={onClickHandle}>{loggedIn ? "Logout" : "Login"}</button>
+
           <Route path="/" exact render={() => {
             return <h1>Welcome React Project</h1>
           }} />
           <Route path="/elements" exact component={ReactElementComponent} />
           <Route path="/user/:firstname/:lastname" exact render={({match}) => {
-            return <h1>Welcome {match.params.firstname} {match.params.lastname}</h1>
-          }}  />
+            return loggedIn ? (
+              <h1>Welcome {match.params.firstname} {match.params.lastname}</h1>
+            ) : (<Redirect to="/" />);
+            }}  
+          />
         </header>
       </div>
     </BrowserRouter>    
